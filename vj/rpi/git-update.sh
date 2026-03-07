21ff28adf8da54e175af18b7f7cc72607471a3ca
Frederic Triquet <17158782+frederictriquet@users.noreply.github.com>
2026-03-07 18:49:26 +0100

    feat: add git update script and desktop entry for VJ Panel launch

---
+++ vj/rpi/git-update.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash                                                                                                                                                   
+cd /home/fred/Jukebox                                                                                                                                                 
+git pull
+echo "Appuie sur Entrée pour fermer..."
+read
